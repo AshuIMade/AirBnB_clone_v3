751d669d07b6f80bf358a03937262c90f9fc5dc3
AsheIMade <asheimade@gmail.com>
2023-05-30 09:54:04 -0700

    first endpoint

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""flask app server"""
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """closes or handles the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    app.run(host=host, port=port, threaded=True)
